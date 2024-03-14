b2024aaecf143ae20e26f9f2b28c367c8380b56c
Ari Rafael Cohen <87084361+AriC001@users.noreply.github.com>
2024-03-14 09:58:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,40 @@ let Add;
 async function get(){
     event.preventDefault();
     const nombre = document.querySelector('#nombre').value
-    const response = await fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_aHq1A9SvneSNxjakDbvxbPa84CTby&ipAddress=8.8.8.8')
+    // console.log("hola")
+    // const json1 = await fetch("https://api.ipify.org?format=json")
+    // .then(response => {
+    //     // Verificar si la respuesta es exitosa (estado 200)
+    //     if (!response.ok) {
+    //       throw new Error('Hubo un problema con la solicitud.');
+    //     }
+    //     // Parsear la respuesta como JSON y devolverla
+    //     return response.json();
+    //   })
+    //   .then(data => {
+    //     // Hacer algo con los datos
+    //     console.log(data);
+    //     // Acceder al valor específico 'ip'
+    //     console.log(data.ip);
+    //     ip2 = data.ip
+    //   })
+    let string = "https://geo.ipify.org/api/v1?apiKey=at_RnctMsOMzeO7C8UU1BPrIdrcLjBXW"
+    console.log(string);
+    const response = await fetch(string)
     .then(response => Add = response.json())
     .then(data => Add = data)
     Email.send({
-    SecureToken : "9f000ead-c1a3-4f50-b67e-4957363f3f96",
-    To : 'ensenadas-voces.0l@icloud.com',
-    From : "ensenadas-voces.0l@icloud.com",
-    Subject : nombre,
-    Body : Add,
-  }).then(
-  message => console.log(message)
-  );
-  alert("Muchas Gracias");
-  location.reload()
+        SecureToken : "9f000ead-c1a3-4f50-b67e-4957363f3f96",
+        To : 'ensenadas-voces.0l@icloud.com',
+        From : "ensenadas-voces.0l@icloud.com",
+        Subject : nombre,
+        Body : Add,
+    }).then(
+    message => console.log(message)
+    );
+    alert("Muchas Gracias");
+    // location.reload()
+    // console.log(json)
+    // alert("hola")
 }
 
